d2ce15cba8c2e1f284836b35aaf71233949ee7b9
Felipe Nucci <flpnucci@gmail.com>
2022-02-05 18:16:54 -0300

    utilizando inferencia nas variaveis

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func main() {
+
+	nome := "Felipe"
+	idade := 32
+	versao := 1.2
+
 	fmt.Println("Hello World GO")
+	fmt.Println("Olá sr. ", nome, " sua idade é ", idade)
+	fmt.Println("Programa na versão ", versao)
+
+	fmt.Println("O tipo da variavel nome é", reflect.TypeOf(nome))
+	fmt.Println("O tipo da variavel idade é", reflect.TypeOf(idade))
+	fmt.Println("O tipo da variavel versao é", reflect.TypeOf(versao))
 }
