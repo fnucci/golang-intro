f4b73792093f36a119fb8770f014f1a68dbf3f5c
Felipe Nucci <flpnucci@gmail.com>
2022-02-21 10:30:36 -0300

    trabalhando com arrays e sllices

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -86,4 +86,21 @@ func iniciarMonitoramento() {
 	} else {
 		fmt.Println("Site", site, "falhou no carregamento com o status code:", resposta.StatusCode, "com erro: ", error)
 	}
+
+	var sites [5]string
+	sites[0] = "https://www.alura.com.br"
+	sites[1] = "https://www.caelum.com.br"
+	sites[2] = "https://www.youtube.com.br"
+	sites[3] = "https://www.twitch.com.br"
+	sites[4] = "https://www.gmail.com.br"
+	fmt.Println(sites)
+
+	exibeNomesComSlices()
+}
+
+func exibeNomesComSlices() {
+	nomes := []string{"Felipe", "Adriana", "Edileia", "Ricardo"}
+	fmt.Println(len(nomes))
+	nomes = append(nomes, "Antonio", "Maria")
+	fmt.Println(cap(nomes))
 }
